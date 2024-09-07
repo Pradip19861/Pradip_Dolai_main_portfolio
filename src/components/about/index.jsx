4c39c805247e55d9663baa7cd3b7129aaf4c9147
PRADIP <pradipdolai03@gmail.com>
2024-09-07 23:09:36 +0530

    hh

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -84,7 +84,7 @@ const AboutDetails = () => {
 
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
           <Link
-            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
+            href="https://github.com/Pradip19861/Next.js-Creative-Portfolio-Website-main"
             target="_blank"
             className="w-full"
           >
